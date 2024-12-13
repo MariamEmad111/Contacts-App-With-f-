c5282d3a84e9e4cc1445d27eb86aed31fe48e4c5
MariamEmad111 <105168089+MariamEmad111@users.noreply.github.com>
2024-12-13 17:15:38 +0200

    commit the implementation of <DataTypes>

--- ConsoleApp4/DataTypes.fs
+++ ConsoleApp4/DataTypes.fs
@@ -1,3 +1,26 @@
 ﻿module DataTypes
 
 // ---- That module will contain all necessary Data Types And Data Structures -----
+namespace ContactManagement
+
+open System.Drawing
+
+// Immutable domain model for Contact
+type Contact = {
+    Name: string
+    PhoneNumber: string
+    Email: string
+    Color: string
+}
+
+
+// Discriminated union for search results
+type ContactSearchResult = 
+    | Exact of Contact
+    | Partial of Contact list
+    | NoMatch
+
+type AppState = {
+    Contacts: Map<string, Contact>
+    SearchQuery: string option
+}
